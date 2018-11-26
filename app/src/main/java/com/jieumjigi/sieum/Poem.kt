a47d1db6c8feff8face50ef2e43c05768f27fb2e
Jeongah Shin <jeongah.h.shin@gmail.com>
2018-11-26 22:49:52 +0900

    dependencies for aac, rx, stetho, retrofit added / model module added

---
+++ app/src/main/java/com/jieumjigi/sieum/Poem.kt
@@ -0,0 +1,10 @@
+package com.jieumjigi.sieum
+
+data class Poem (
+        var reservation_date : String,
+        var title : String,
+        var content : String,
+        var abbrev : String,
+        var register_date : String,
+        var uid : String
+)
